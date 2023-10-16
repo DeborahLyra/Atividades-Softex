05a8db8f2c8bf124be0c3c641ce3882382681cf3
Deborah Lyra <deborahp.lyra@gmail.com>
2023-10-16 20:53:08 -0300

    atividade primeiro componente

--- iniciando-react/src/App.js
+++ iniciando-react/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MSG from './components/msg/msg';
 
 function App() {
   return (
     <div>
     <h1>Deborah</h1>
+    <MSG/>
     </div>
   );
 }
---
+++ iniciando-react/src/components/msg/msg.js
@@ -0,0 +1,5 @@
+function msg (){
+   return <h2>Mensagem da pasta msg</h2>
+}
+
+export default msg
