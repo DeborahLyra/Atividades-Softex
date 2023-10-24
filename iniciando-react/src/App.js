cfa8464f9ee568cc66d8ccd9b1bd6c8e05fbb7d2
Deborah Lyra <deborahp.lyra@gmail.com>
2023-10-24 15:13:29 -0300

    iniciando props

--- iniciando-react/src/App.js
+++ iniciando-react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MSG from './components/msg/msg';
+import Music from './components/music/music';
 
 function App() {
   return (
     <div>
     <h1>Deborah</h1>
     <MSG/>
+    <Music color= 'purple'></Music>
     </div>
   );
 }
---
+++ iniciando-react/src/components/music/music.jsx
@@ -0,0 +1,16 @@
+function Music ({color}){
+    return (
+        <div style={{color: color}}>
+          <h1>Ours by TS</h1>
+            <p>
+                So don't you worry your pretty little mind <br></br>
+                People throw rocks at things that shine<br></br>
+                And life makes love look hard<br></br>
+                The stakes are high, the water's rough<br></br>
+                But this love is ours
+            </p>
+        </div>
+    )
+}
+
+export default Music;
